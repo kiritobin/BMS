40bc041b2ea551708dcfea0c90976f139239aa90
周文慧 <1305419069@qq.com>
2018-09-16 12:33:00 +0800

    实体提交

---
+++ bms.Model/SingleHead.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bms.Model
+{
+    public class SingleHead
+    {
+        private int singleHeadId;
+        private DateTime time;
+        private Region regionId;
+        private User userId;
+        private User userName;
+        private int allBillCount;
+        private double allTotalPrice;
+        private double allRealPrice;
+        private int type;
+
+        /// <summary>
+        /// 单头id
+        /// </summary>
+        public int SingleHeadId { get => singleHeadId; set => singleHeadId = value; }
+        /// <summary>
+        /// 制单时间
+        /// </summary>
+        public DateTime Time { get => time; set => time = value; }
+        /// <summary>
+        /// 地区id
+        /// </summary>
+        public Region RegionId { get => regionId; set => regionId = value; }
+        /// <summary>
+        /// 用户id
+        /// </summary>
+        public User UserId { get => userId; set => userId = value; }
+        /// <summary>
+        /// 用户名称
+        /// </summary>
+        public User UserName { get => userName; set => userName = value; }
+        /// <summary>
+        /// 单据总数量
+        /// </summary>
+        public int AllBillCount { get => allBillCount; set => allBillCount = value; }
+        /// <summary>
+        /// 总码洋
+        /// </summary>
+        public double AllTotalPrice { get => allTotalPrice; set => allTotalPrice = value; }
+        /// <summary>
+        /// 总实洋
+        /// </summary>
+        public double AllRealPrice { get => allRealPrice; set => allRealPrice = value; }
+
+        public int Type { get => type; set => type = value; }
+
+        /// <summary>
+        /// 单头无参构造函数
+        /// </summary>
+        public SingleHead() { }
+
+        /// <summary>
+        /// 单头参数构造函数
+        /// </summary>
+        /// <param name="singleHeadId">单头id</param>
+        /// <param name="time">制单时间</param>
+        /// <param name="regionId">地区id'</param>
+        /// <param name="userId">用户id</param>
+        /// <param name="userName">用户名</param>
+        /// <param name="allBillCount">单据总数量</param>
+        /// <param name="allTotalPrice">总码洋</param>
+        /// <param name="allRealPrice">总实洋</param>
+        /// <param name="type">状态（0为出库，1为入库）</param>
+        public SingleHead(int singleHeadId, DateTime time, Region regionId, User userId, User userName, int allBillCount, double allTotalPrice, double allRealPrice, int type)
+        {
+            this.SingleHeadId = singleHeadId;
+            this.Time = time;
+            this.RegionId = regionId;
+            this.UserId = userId;
+            this.UserName = userName;
+            this.AllBillCount = allBillCount;
+            this.AllTotalPrice = allTotalPrice;
+            this.AllRealPrice = allRealPrice;
+            this.type = type;
+        }
+    }
+}
---
+++ bms.Model/Stock.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bms.Model
+{
+    public class Stock
+    {
+        private int stockId;
+        private int stockNum;
+        private BookBasicData bookNum;
+        private BookBasicData iSBN;
+        private Region regionId;
+        private GoodsShelves goodsShelvesId;
+        /// <summary>
+        /// 库存id
+        /// </summary>
+        public int StockId { get => stockId; set => stockId = value; }
+        /// <summary>
+        /// 库存名称
+        /// </summary>
+        public int StockNum { get => stockNum; set => stockNum = value; }
+        /// <summary>
+        /// 书号
+        /// </summary>
+        public BookBasicData BookNum { get => bookNum; set => bookNum = value; }
+        /// <summary>
+        /// 国际书号
+        /// </summary>
+        public BookBasicData ISBN { get => iSBN; set => iSBN = value; }
+        /// <summary>
+        /// 地区id
+        /// </summary>
+        public Region ReginId { get => regionId; set => reginId = value; }
+        /// <summary>
+        /// 货架id
+        /// </summary>
+        public GoodsShelves GoodsShelvesId { get => goodsShelvesId; set => goodsShelvesId = value; }
+
+        /// <summary>
+        /// 库存无参构造函数
+        /// </summary>
+        public Stock() { }
+
+        /// <summary>
+        /// 库存参数构造函数
+        /// </summary>
+        /// <param name="stockId">库存id</param>
+        /// <param name="stockNum">库存名称</param>
+        /// <param name="bookNum">书号</param>
+        /// <param name="iSBN">国际书号</param>
+        /// <param name="regionId">地区id</param>
+        /// <param name="goodsShelvesId">货架id</param>
+        public Stock(int stockId, int stockNum, BookBasicData bookNum, BookBasicData iSBN, Region regionId, GoodsShelves goodsShelvesId)
+        {
+            this.stockId = stockId;
+            this.stockNum = stockNum;
+            this.bookNum = bookNum;
+            this.iSBN = iSBN;
+            this.reginId = reginId;
+            this.goodsShelvesId = goodsShelvesId;
+        }
+    }
+}
