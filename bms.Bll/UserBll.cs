2cdf4012d3ea544275c6973f6d69e1552b72f6b5
周文慧 <1305419069@qq.com>
2018-09-17 14:12:41 +0800

    查找用户方法提交

---
+++ bms.Bll/UserBll.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using bms.Dao;
+
+namespace bms.Bll
+{
+    public class UserBll
+    {
+        UserDao userDao = new UserDao();
+        /// <summary>
+        /// 获取所有用户信息
+        /// </summary>
+        /// <returns>数据集</returns>
+        public DataSet select()
+        {
+            return userDao.select();
+        }
+
+        /// <summary>
+        /// 根据地区id获取用户信息
+        /// </summary>
+        /// <param name="regionId">地区id</param>
+        /// <returns>数据集</returns>
+        public DataSet selectByRegion(int regionId)
+        {
+            return userDao.selectByRegion(regionId);
+        }
+    }
+}
---
+++ bms.Dao/UserDao.cs
@@ -0,0 +1,52 @@
+﻿using bms.DBHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace bms.Dao
+{
+    public class UserDao
+    {
+        MySqlHelp db = new MySqlHelp();
+        /// <summary>
+        /// 获取所有用户信息
+        /// </summary>
+        /// <returns></returns>
+        public DataSet select()
+        {
+            string comText = "select userId,userName,regionId,roleId from T_User";
+            DataSet ds = db.FillDataSet(comText, null, null);
+            if (ds != null || ds.Tables[0].Rows.Count > 0)
+            {
+                return ds;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 根据地区id获取用户信息
+        /// </summary>
+        /// <param name="regionId">地区id</param>
+        /// <returns></returns>
+        public DataSet selectByRegion(int regionId)
+        {
+            string comText = "select userId,userName,regionId,roleId from T_User where regionId=@regionId";
+            string[] param = { "@regionId" };
+            object[] values = { regionId };
+            DataSet ds = db.FillDataSet(comText, null, null);
+            if (ds != null || ds.Tables[0].Rows.Count > 0)
+            {
+                return ds;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
