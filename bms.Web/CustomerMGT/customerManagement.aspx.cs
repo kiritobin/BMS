a0f05a825e3af9c98b09046dc0b9479e76d3c691
mz <mz@DESKTOP-LUO88QN>
2018-09-19 13:12:00 +0800

    客户信息数据绑定修改

--- bms.Web/CustomerMGT/customerManagement.aspx.cs
+++ bms.Web/CustomerMGT/customerManagement.aspx.cs
@@ -10,226 +10,95 @@ using System.Web.UI.WebControls;
 
 namespace bms.Web.CustomerMGT
 {
+    using System.Text;
     using Result = Enums.OpResult;
     public partial class customerManagement : System.Web.UI.Page
     {
-        protected DataSet ds = null;//获取客户数据集
-        protected DataSet regionDs = null;//获取地区数据集
-        protected int getCurrentPage;//当前页
-        protected int totalPage;//总页数
-        protected int pagesize = 4;
-        protected string searchRegion;//下拉查询
-        protected string showStr;//下拉查询
-        protected string strWhere;//输入框查询
-        protected string op;//请求ajax传入的op值
-        RSACryptoService RSAC = new RSACryptoService();
-        CustomerBll cBll = new CustomerBll();
-        RegionBll reBll = new RegionBll();
+        //protected DataSet ds = null;//获取客户数据集
+        //protected DataSet regionDs = null;//获取地区数据集
+        //protected int getCurrentPage;//当前页
+        //protected int totalPage;//总页数
+        //protected int pagesize = 4;
+        //protected string searchRegion;//下拉查询
+        //protected string showStr;//下拉查询
+        //protected string strWhere;//输入框查询
+        //protected string op;//请求ajax传入的op值
+        //RSACryptoService RSAC = new RSACryptoService();
+        //CustomerBll cBll = new CustomerBll();
+        //RegionBll reBll = new RegionBll();
+
+        public int totalCount, intPageCount;
+        public DataSet regionDs, ds;
+        CustomerBll cbll = new CustomerBll();
+        RegionBll rbll = new RegionBll();
+        UserBll userbll = new UserBll();
         protected void Page_Load(object sender, EventArgs e)
         {
-            searchRegion = Request.QueryString["regionID"];
-            strWhere = Request.QueryString["strWhere"];
-            op = Context.Request["op"];
+            getData();
+            //searchRegion = Request.QueryString["regionID"];
+            //strWhere = Request.QueryString["strWhere"];
+            //op = Context.Request["op"];
+        }
+        public string getData()
+        {
+            //获取分页数据
+            int currentPage = Convert.ToInt32(Request["page"]);
+            if (currentPage == 0)
+            {
+                currentPage = 1;
+            }
+            string search = Request["search"];
+            string region = Request["region"];
+            if (region == "" || region == null)
+            {
+                search = String.Format("customerID {0} or customerName {0} or regionName {0}", "like " + "'%" + search + "%'");
+            }
+            else if (search == "" || search == null)
+            {
+                String.Format("regionId={0}", "'" + region + "'");
+            }
+            else if((region == "" || region == null)&& (search == "" || search == null))
+            {
+                search = "";
+            }
+            else if(search != null || search != "")
+            {
+                search = String.Format("customerID {0} or customerName {0} or regionName {0}", " like " + "'%" + search + "%'");
+            }
 
-            if (!IsPostBack)
+            TableBuilder tb = new TableBuilder();
+            tb.StrTable = "V_Customer";
+            tb.OrderBy = "customerID";
+            tb.StrColumnlist = "customerID,customerName,regionId,regionName";
+            tb.IntPageSize = 3;
+            tb.IntPageNum = currentPage;
+            tb.StrWhere = search;
+            //获取展示的客户数据
+            ds = userbll.selectByPage(tb, out totalCount, out intPageCount);
+            //获取地区下拉数据
+            regionDs = rbll.select();
+            //生成table
+            StringBuilder strb = new StringBuilder();
+            strb.Append("<tbody>");
+            for(int i = 0; i < ds.Tables[0].Rows.Count; i++)
             {
-                getData("");
+                strb.Append("<tr><td>" + (i + 1 + ((currentPage - 1) * 3)) + "</td>");
+                strb.Append("<td>" + ds.Tables[0].Rows[i]["customerID"].ToString() + "</td>");
+                strb.Append("<td>" + ds.Tables[0].Rows[i]["customerName"].ToString() + "</td>");
+                //strb.Append("<td>" + ds.Tables[0].Rows[i]["regionId"].ToString() + "</td>");
+                strb.Append("<td>" + ds.Tables[0].Rows[i]["regionName"].ToString() + "</td>");
+                strb.Append("<td>" + "<button type='button' class='btn btn-default btn-sm reset_pwd'>" + "重置密码" + "</button>" + " </td>");
+                strb.Append("<td>" + "<button class='btn btn-warning btn-sm btn_Editor' data-toggle='modal' data-target='#myModa2'>" + "<i class='fa fa-pencil fa-lg'></i>" + "&nbsp 编辑" + "</button>");
+                strb.Append("<button class='btn btn-danger btn-sm btn_delete'>" + "<i class='fa fa-trash-o fa-lg'></i>&nbsp 删除" + "</button>" + " </td></tr>");
             }
-            if (searchRegion != "null" && searchRegion!="" && searchRegion != null)
+            strb.Append("</tbody>");
+            string op = Request["op"];
+            if (op == "paging")
             {
-                getData(SearchRegion());
-            }
-            if (strWhere != null)
-            {
-                getData(SearchStrWhere());
-            }
-            if (op == "add")
-            {
-                InsertCustomer();
-            }
-            if(op== "editData")
-            {
-                UpdateCustomer();
-            }
-            if (op == "del")
-            {
-                DeleteCustomer();
-            }
-            if(op== "reset")
-            {
-                RestPwd();
-            }
-        }
-        /// <summary>
-        /// 获取基础数据
-        /// </summary>
-        public void getData(String strWhere)
-        {
-            string currentPage = Request.QueryString["currentPage"];
-            if (currentPage == null || currentPage.Length < 0)
-            {
-                currentPage = "1";
-            }
-            regionDs = reBll.select();
-            TableBuilder tBuilder = new TableBuilder()
-            {
-                StrTable = "V_Customer",
-                StrColumnlist = "customerID,customerName,regionId,regionName",
-                OrderBy = "customerID",
-                StrWhere = strWhere,
-                IntPageNum = int.Parse(currentPage),
-                IntPageSize = pagesize
-            };
-            int totalCount;
-            int pageCount;
-            ds = cBll.selectByPage(tBuilder, out totalCount, out pageCount);
-            getCurrentPage = int.Parse(currentPage);
-            totalPage = pageCount;
-        }
-        /// <summary>
-        /// 地区下拉查询
-        /// </summary>
-        /// <returns></returns>
-        public string SearchRegion()
-        {
-            try
-            {
-               // searchRegion = Request.QueryString["strWhere"];
-                if (searchRegion.Length == 0 || searchRegion == "" || searchRegion == "0")
-                {
-                    searchRegion = "";
-                }
-                else
-                {
-                    showStr = searchRegion;
-                    searchRegion = String.Format("regionId={0}", "'" + searchRegion + "'");
-                }
-            }
-            catch { }
-            return searchRegion;
-        }
-
-        /// <summary>
-        /// 输入框查询
-        /// </summary>
-        /// <returns></returns>
-        public string SearchStrWhere()
-        {
-            if (strWhere.Length == 0 || strWhere == "" || strWhere == "")
-            {
-                strWhere = "";
-            }
-            else
-            {
-                strWhere = String.Format("customerID {0} or customerName {0} or regionName {0}", "like " + "'%" + strWhere + "%'");
-            }
-            return strWhere;
-        }
-        /// <summary>
-        /// 添加客户
-        /// </summary>
-        public void InsertCustomer()
-        {
-            string custId = Context.Request["customerId"],
-                custName = Context.Request["cutomerName"],
-                regId = Context.Request["zoneId"],
-                pwd = "000000";
-            string custPwd = RSAC.Encrypt(pwd);
-            bool isCustomerId = cBll.SelectById(custId);
-            if (isCustomerId == true)
-            {
-                Response.Write("该账号已经存在");
+                Response.Write(strb.ToString());
                 Response.End();
             }
-            else
-            {
-                Region reg = new Region()
-                {
-                    RegionId = int.Parse(regId)
-                };
-                Customer cust = new Customer()
-                {
-                    CustomerId = Convert.ToInt32(custId),
-                    CustomerName = custName,
-                    CustomerPwd = custPwd,
-                    RegionId = reg
-                };
-                Result result = cBll.Insert(cust);
-                if (result == Result.添加成功)
-                {
-                    Response.Write("添加成功");
-                    Response.End();
-                }
-                else
-                {
-                    Response.Write("添加失败");
-                    Response.End();
-                }
-            }
-        }
-        /// <summary>
-        /// 更新客户信息
-        /// </summary>
-        public void UpdateCustomer()
-        {
-            string custId = Context.Request["customerId"];
-            string custName = Context.Request["customername"];
-            string regId = Context.Request["regionid"];
-            Region reg = new Region()
-            {
-                RegionId = int.Parse(regId)
-            };
-            Customer customer = new Customer()
-            {
-                CustomerId = Convert.ToInt32(custId),
-                CustomerName = custName,
-                RegionId = reg
-            };
-            Result row = cBll.update(customer);
-            if(row == Result.更新成功)
-            {
-                Response.Write("更新成功");
-                Response.End();
-            }
-            else
-            {
-                Response.Write("更新失败");
-                Response.End();
-            }
-        }
-        /// <summary>
-        /// 删除用户
-        /// </summary>
-        public void DeleteCustomer()
-        {
-            string customerID = Context.Request["cutomerId"];
-            Result row = cBll.Delete(int.Parse(customerID));
-            if (row == Result.删除成功)
-            {
-                Response.Write("删除成功");
-                Response.End();
-            }
-            else
-            {
-                Response.Write("删除失败");
-                Response.End();
-            }
-        }
-        public void RestPwd()
-        {
-            string customerId = Context.Request["customerid"];
-            string pwd = RSAC.Encrypt("000000");
-            Result result = cBll.ResetPwd(int.Parse(customerId), pwd);
-            if(result == Result.更新成功)
-            {
-                Response.Write("重置成功");
-                Response.End();
-            }
-            else
-            {
-                Response.Write("重置失败");
-                Response.End();
-            }
+            return strb.ToString();
         }
     }
 }
