fcf4a09ecf26254eaddefd365f6731c2f419bde2
mz <mz@DESKTOP-LUO88QN>
2018-09-22 16:52:07 +0800

    地区表、货架表联动插入方法

---
+++ bms.Model/TableInsertion.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bms.Model
+{
+    public class TableInsertion
+    {
+        private string inRegionName;
+        private string inShelvesName;
+        private int outCount;
+
+        public TableInsertion()
+        {
+        }
+
+        public TableInsertion(string inRegionName, string inShelvesName, int outCount)
+        {
+            this.inRegionName = inRegionName;
+            this.inShelvesName = inShelvesName;
+            this.outCount = outCount;
+        }
+
+        public string InRegionName
+        {
+            get
+            {
+                return inRegionName;
+            }
+
+            set
+            {
+                inRegionName = value;
+            }
+        }
+
+        public string InShelvesName
+        {
+            get
+            {
+                return inShelvesName;
+            }
+
+            set
+            {
+                inShelvesName = value;
+            }
+        }
+
+        public int OutCount
+        {
+            get
+            {
+                return outCount;
+            }
+
+            set
+            {
+                outCount = value;
+            }
+        }
+    }
+}
