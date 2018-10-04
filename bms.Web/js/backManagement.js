dcb49bffcd0679b0bef808a09c5e0eae0eb464ae
mz <mz@DESKTOP-LUO88QN>
2018-10-04 14:44:00 +0800

    销退更改

---
+++ bms.Web/js/backManagement.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    $(".paging").pagination({
+        pageCount: $("#intPageCount").val(), //总页数
+        jump: true,
+        mode: 'fixed',//固定页码数量
+        coping: true,
+        homePage: '首页',
+        endPage: '尾页',
+        prevContent: '上页',
+        nextContent: '下页',
+        callback: function (api) {
+            var search = $("#btn-search").val().trim();
+            $.ajax({
+                type: 'Post',
+                url: 'customerManagement.aspx',
+                data: {
+                    page: api.getCurrent(), //页码
+                    search: search,
+                    op: "paging"
+                },
+                dataType: 'text',
+                success: function (data) {
+                    $("#table tr:not(:first)").empty(); //清空table处首行
+                    $("#table").append(data); //加载table
+                }
+            });
+        }
+    });
+})
