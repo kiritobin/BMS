3f52e6831de87a8223fb05b1536e8e14f6a99545
jock <jock@DESKTOP-I2S4DFK>
2018-10-09 10:34:05 +0800

    销售任务添加销退

--- bms.Web/SalesMGT/tradeManagement.aspx.cs
+++ bms.Web/SalesMGT/tradeManagement.aspx.cs
@@ -121,52 +121,14 @@ namespace bms.Web.SalesMGT
                 Response.Write("添加成功");
                 Response.End();
             }
-            ////添加销退
-            //if (op == "saleback")
-            //{
-            //    string saleTaskId = Request["ID"];
-            //    SaleTask sale = saleBll.selectById(saleTaskId);
-            //    User user = new User();
-            //    user.UserId = sale.UserId;//用户Id
-            //    sellOffHeadBll sellBll = new sellOffHeadBll();
-            //    int count = sellBll.getCount(saleTaskId);
-            //    string sellId;
-            //    if (count > 0)
-            //    {
-            //        count += 1;
-            //        sellId = "XT" + DateTime.Now.ToString("yyyyMMdd") + count.ToString().PadLeft(6, '0');
-            //        Session["sell"] = sellId;
-            //    }
-            //    else
-            //    {
-            //        count = 1;
-            //        sellId = "XT" + DateTime.Now.ToString("yyyyMMdd") + count.ToString().PadLeft(6, '0');
-            //        Session["sell"] = sellId;
-            //    }
-            //    DateTime time = DateTime.Now;
-            //    SaleTask st = new SaleTask()
-            //    {
-            //        SaleTaskId = saleTaskId
-            //    };
-            //    SellOffHead sell = new SellOffHead()
-            //    {
-            //        SellOffHeadId = sellId,
-            //        SaleTaskId = st,
-            //        MakingTime = time,
-            //        User = user
-            //    };
-            //    Result row = sellBll.Insert(sell);
-            //    if (row == Result.添加成功)
-            //    {
-            //        Response.Write("添加成功");
-            //        Response.End();
-            //    }
-            //    else
-            //    {
-            //        Response.Write("添加失败");
-            //        Response.End();
-            //    }
-            //}
+            //添加销退
+            if (op == "saleback")
+            {
+                string saleTaskId = Request["ID"];
+                Session["saleId"] = saleTaskId;
+                Response.Write("yes");
+                Response.End();
+            }
         }
         /// <summary>
         /// 获取基础数据及查询方法
