3545da04e0ee5049b5c73e37e1b9914d471be488
mz <mz@DESKTOP-LUO88QN>
2018-10-24 09:38:30 +0800

    LOGO更改

--- bms.Web/js/retailManagement.js
+++ bms.Web/js/retailManagement.js
@@ -1,4 +1,32 @@
-﻿$(document).ready(function () {
+﻿//退出系统
+function logout() {
+    swal({
+        title: "温馨提示:)",
+        text: "您确定要退出系统吗？",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        confirmButtonClass: 'btn btn-success',
+        cancelButtonClass: 'btn btn-danger',
+        buttonsStyling: false,
+        allowOutsideClick: false    //用户无法通过点击弹窗外部关闭弹窗
+    }).then(function () {
+        $.ajax({
+            type: 'get',
+            url: 'retailManagement.aspx?op=logout',
+            datatype: 'text',
+            data: {},
+            success: function (data) {
+                window.location.href = "../login.aspx";
+            }
+        });
+    })
+}
+
+$(document).ready(function () {
     $(".paging").pagination({
         pageCount: $("#intPageCount").val(), //总页数
         jump: true,
