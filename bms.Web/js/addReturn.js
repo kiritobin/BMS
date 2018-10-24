6aebdd895223191513f63d41d73270cdfb78358c
daobin <daobin@DAOBIN-PC>
2018-10-24 15:55:08 +0800

    gg

--- bms.Web/js/addReturn.js
+++ bms.Web/js/addReturn.js
@@ -31,9 +31,9 @@ function logout() {
 
 //只允许数字
 $("#table").delegate(".isbn", "keyup", function (e) {
-    $(this).val($(this).val().replace(/[^0-9.]/g, ''));
+    $(this).val($(this).val().replace(/[^\w\.\/]/ig, ''));
 }).bind("paste", function () {  //CTR+V事件处理    
-    $(this).val($(this).val().replace(/[^0-9.]/g, ''));
+    $(this).val($(this).val().replace(/[^\w\.\/]/ig, ''));
 }).css("ime-mode", "disabled");
 $("#table").delegate(".count", "keyup", function (e) {
     $(this).val($(this).val().replace(/[^0-9.]/g, ''));
