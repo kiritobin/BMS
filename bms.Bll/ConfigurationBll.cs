a2a19c03267e2bf4485561ddfa9c7db6cee5c4b4
jock <jock@DESKTOP-I2S4DFK>
2018-11-22 19:52:33 +0800

    大屏配置更改

---
+++ bms.Bll/ConfigurationBll.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using bms.Dao;
+
+namespace bms.Bll
+{
+    using Result = Enums.OpResult;
+    public class ConfigurationBll
+    {
+        ConfigurationDao dao = new ConfigurationDao();
+        public Result Insert(DateTime startTime,DateTime endTime,string regionName)
+        {
+            int row = dao.Insert(startTime, endTime, regionName);
+            if (row > 0)
+            {
+                return Result.添加成功;
+            }
+            else
+            {
+                return Result.添加失败;
+            }
+        }
+        public Result Update(DateTime startTime, DateTime endTime, string regionName)
+        {
+            int row = dao.Update(startTime, endTime, regionName);
+            if (row > 0)
+            {
+                return Result.更新成功;
+            }
+            else
+            {
+                return Result.更新失败;
+            }
+        }
+        public Result isExist(string regionName)
+        {
+            int count = dao.isExist(regionName);
+            if (count > 0)
+            {
+                return Result.记录存在;
+            }
+            else
+            {
+                return Result.记录不存在;
+            }
+        }
+    }
+}
