f83096c7d52e97118e17a5f5d44033e180421bfe
jock <jock@DESKTOP-I2S4DFK>
2018-11-26 10:24:24 +0800

    添加销售统计报表Dao Bll

---
+++ bms.Dao/salesStatisticsDao.cs
@@ -0,0 +1,62 @@
+﻿using bms.DBHelper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace bms.Dao
+{
+    public class salesStatisticsDao
+    {
+        MySqlHelp db = new MySqlHelp();
+       //导出当前查询
+        public DataSet exportTotal()
+        {
+            String cmdText = "";
+            string[] param = { };
+            object[] values = { };
+            DataSet ds = db.FillDataSet(cmdText, param, values);
+            if (ds != null && ds.Tables[0].Rows.Count > 0)
+            {
+                return ds;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        //导出所有明细
+        public DataSet exportAllDetails()
+        {
+            String cmdText = "";
+            string[] param = { };
+            object[] values = { };
+            DataSet ds = db.FillDataSet(cmdText, param, values);
+            if (ds != null && ds.Tables[0].Rows.Count > 0)
+            {
+                return ds;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        //导出单个明细
+        public DataSet exportDetails()
+        {
+            String cmdText = "";
+            string[] param = { };
+            object[] values = { };
+            DataSet ds = db.FillDataSet(cmdText, param, values);
+            if (ds != null && ds.Tables[0].Rows.Count > 0)
+            {
+                return ds;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
