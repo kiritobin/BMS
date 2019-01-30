24ec2bad19c8efe1f3ae8d5f77f4857e314335a9
zhouwhui <qc_d9bcefc35c9a49a0b26d7792f8993094@git.code.tencent.com>
2019-01-30 21:32:34 +0800

    出入退统计

--- bms.Dao/WarehousingDao.cs
+++ bms.Dao/WarehousingDao.cs
@@ -361,7 +361,8 @@ namespace bms.Dao
 
         public DataSet getKinds(int type,string where)
         {
-            string cmdText = "SELECT SUM(NUMBER) AS sl,COUNT(DISTINCT bookNum) as pz,sum(realPrice) as sy,sum(totalPrice) as my FROM v_monomer WHERE "+where+" and SINGLEHEADID IN (SELECT singleHeadId FROM t_singlehead WHERE TYPE=@type)";
+            //string cmdText = "SELECT SUM(NUMBER) AS sl,COUNT(DISTINCT bookNum) as pz,sum(realPrice) as sy,sum(totalPrice) as my FROM v_monomer WHERE "+where+" and SINGLEHEADID IN (SELECT singleHeadId FROM t_singlehead WHERE TYPE=@type)";
+            string cmdText = "SELECT SUM(NUMBER) AS sl,COUNT(DISTINCT bookNum) as pz,sum(realPrice) as sy,sum(totalPrice) as my FROM v_monomer WHERE " + where + " and  TYPE=@type";
             string[] param = { "@type" };
             object[] values = { type };
             DataSet ds = db.FillDataSet(cmdText, param, values);
