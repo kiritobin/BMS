57e3edd34abe711279d4b94f238dcb61e1f46340
jock <jock@DESKTOP-I2S4DFK>
2019-03-13 10:24:15 +0800

    销售导出添加进货折扣

--- bms.Bll/SalesDetailsBll.cs
+++ bms.Bll/SalesDetailsBll.cs
@@ -40,6 +40,7 @@ namespace bms.Bll
             excel.Columns.Add("码洋");
             excel.Columns.Add("实洋");
             excel.Columns.Add("销售折扣");
+            excel.Columns.Add("进货折扣");
             excel.Columns.Add("供应商");
             excel.Columns.Add("采集时间");
             excel.Columns.Add("采集人");
@@ -55,7 +56,7 @@ namespace bms.Bll
                 foreach (DataRow row in count)
                 {
                     string bookName = ToDBC(row[2].ToString());
-                    excel.Rows.Add(row[0], row[1], bookName, row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15]);
+                    excel.Rows.Add(row[0], row[1], bookName, row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15],row[16]);
                 }
             }
             return excel;
