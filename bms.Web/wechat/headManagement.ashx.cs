b95eefc2214bb9ddc2394a441205bbcc96805481
jock <jock@DESKTOP-I2S4DFK>
2019-03-20 11:15:53 +0800

    销售单头地区ID修改

--- bms.Web/wechat/headManagement.ashx.cs
+++ bms.Web/wechat/headManagement.ashx.cs
@@ -91,6 +91,9 @@ namespace bms.Web.wechat
         {
             string customeID = context.Request["customID"];
             string saleId = context.Request["saletaskID"];
+            UserBll user = new UserBll();
+            DataSet userds = user.selectByUserId(customeID);
+            int regionId = Convert.ToInt32(userds.Tables[0].Rows[0]["regionId"]);
             SaleHeadBll saleheadbll = new SaleHeadBll();
             SaleHead salehead = new SaleHead();
             string SaleHeadId;
@@ -127,9 +130,9 @@ namespace bms.Web.wechat
                     }
                     else
                     {
-                         getheadId = saleheadbll.getPerSaleHeadIdByTime(nowTime);
+                        getheadId = saleheadbll.getPerSaleHeadIdByTime(nowTime);
                     }
-                        
+
                     if (getheadId == "" || getheadId == null)
                     {
                         count = 1;
@@ -160,7 +163,7 @@ namespace bms.Web.wechat
             salehead.AllTotalPrice = 0;
             salehead.AllRealPrice = 0;
             salehead.UserId = customeID;
-            salehead.RegionId = 67;
+            salehead.RegionId = regionId;
             salehead.DateTime = DateTime.Now.ToLocalTime();
             Result result;
             if (type == "team")
@@ -210,7 +213,7 @@ namespace bms.Web.wechat
                 }
                 else
                 {
-                    result = salemonbll.DeleteHead(saleTaskId, salehead,"");
+                    result = salemonbll.DeleteHead(saleTaskId, salehead, "");
                 }
 
                 if (result == Result.删除成功)
