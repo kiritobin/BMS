9ee2c63660a8f954fbe34a104e7f8dd775ec38f5
daobin <daobin@LAPTOP-KNBEL2U7>
2019-05-20 17:19:49 +0800

    addstock.js

--- bms.Web/js/addStock.js
+++ bms.Web/js/addStock.js
@@ -312,7 +312,7 @@ $("#table").delegate(".count","keypress" ,function (e) {
         var total = $(this).parent().next().next().next();
         var real = $(this).parent().next().next().next().next();
         total.text((count * price).toFixed(2));
-        real.text((count * price * discount).toFixed(2));
+        real.text((count * price * discount*0.01).toFixed(2));
         if (count <= 0) {
             swal({
                 title: "温馨提示:)",
